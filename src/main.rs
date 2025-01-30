20974f5d00e071bc8a11f4f5e51421495776178d
Jonathon Anderson <coding@jpanderson.dev>
2025-01-29 17:01:19 -0800

    Handle vec u8

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
 use base64::prelude::*;
 use clap::{Arg, ArgAction, ArgGroup, Command};
-
-struct Cmd {
-  encoding: bool,
-  decoding: bool,
-  url_safe: bool,
-  text: String,
-}
+use std::str;
 
 fn main() {
   let encode = Arg::new("encode")
     .short('e')
     .long("encode")
     .action(ArgAction::SetTrue)
-    .help("Provide text to base64 encode");
+    .help("Perform base64 encoding");
 
   let decode = Arg::new("decode")
     .short('d')
     .long("decode")
     .action(ArgAction::SetTrue)
-    .help("Provide text to base64 decode");
+    .help("Perform base64 decoding");
 
   let action = ArgGroup::new("action")
     .args(["encode", "decode"])
@@ -42,45 +36,50 @@ fn main() {
   let matches = cmd.get_matches();
 
   let is_encoding = matches.get_flag("encode");
-  let is_decoding = matches.get_flag("decode");
+  // We hilariously don't need to catch this because clap should ensure we
+  // only get either encoding or decoding.
+  // let is_decoding = matches.get_flag("decode");
   let is_url_safe = matches.get_flag("url_safe");
   let passed_text = matches
     .get_one::<String>("text")
     .expect("Must pass text to encode or decode")
     .clone();
 
-  let val = Cmd {
-    encoding: is_encoding,
-    decoding: is_decoding,
-    url_safe: is_url_safe,
-    text: passed_text,
+  let result = if is_encoding {
+    do_encoding(passed_text, is_url_safe)
+  } else {
+    do_decoding(passed_text, is_url_safe)
   };
 
-  match val {
-    Cmd {
-      encoding: true,
-      decoding: _,
-      url_safe: false,
-      text,
-    } => println!("{}", BASE64_STANDARD.encode(text)),
-    Cmd {
-      encoding: _,
-      decoding: true,
-      url_safe: false,
-      text,
-    } => println!("{:?}", BASE64_STANDARD.decode(text)),
-    Cmd {
-      encoding: true,
-      decoding: _,
-      url_safe: true,
-      text,
-    } => println!("{}", BASE64_URL_SAFE.encode(text)),
-    Cmd {
-      encoding: _,
-      decoding: true,
-      url_safe: true,
-      text,
-    } => println!("{:?}", BASE64_URL_SAFE.decode(text)),
-    _ => panic!("Something has gone terribly wrong"),
+  match result {
+    Ok(converted) => println!("{}", converted),
+    Err(error_msg) => {
+      println!("{}", error_msg);
+      std::process::exit(1)
+    }
+  }
+}
+
+fn do_encoding(text: String, url_safe: bool) -> Result<String, String> {
+  if url_safe {
+    Ok(BASE64_URL_SAFE.encode(text))
+  } else {
+    Ok(BASE64_STANDARD.encode(text))
+  }
+}
+
+fn do_decoding(text: String, url_safe: bool) -> Result<String, String> {
+  let decode_attempt = if url_safe {
+    BASE64_URL_SAFE.decode(text)
+  } else {
+    BASE64_STANDARD.decode(text)
+  };
+
+  match decode_attempt {
+    Err(decode_error) => Err(decode_error.to_string()),
+    Ok(vec_data) => match str::from_utf8(&vec_data) {
+      Ok(s) => Ok(String::from(s)),
+      Err(..) => Err("Non-UTF8 data found, use another utility".to_string()),
+    },
   }
 }
